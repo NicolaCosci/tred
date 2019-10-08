5290d802b2a6685690d577b292127be7662512b2
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-08 09:42:46 +0200

    introduzioni treed

---
+++ introduzione tred/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace introduzione_tred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Visualizzapari();
+            Console.WriteLine();
+            Visualizzadispari();
+
+            Console.ReadLine();
+        }
+
+        public static void Visualizzapari()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i+ " ");
+                }
+            }
+            
+        }
+        public static void Visualizzadispari()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.Write(i+ " ");
+                }
+            }
+            
+        }
+    }
+}
